48a39b7c2cda600e3a551f37e2961d534d31221c
eduardo <hceduardo@users.noreply.github.com>
2018-07-10 18:00:08 +0100

    improved query

--- src/main/scala/com/redhat/mavenpop/DependencyComputer/CypherQueries.scala
+++ src/main/scala/com/redhat/mavenpop/DependencyComputer/CypherQueries.scala
@@ -2,9 +2,18 @@ package com.redhat.mavenpop.DependencyComputer
 
 object CypherQueries {
 
-  val GetDependenciesFromList: String =
+  def GetDependenciesFromList = _GetDependenciesFromListv2
+
+  private val _GetDependenciesFromListv1: String =
     """WITH $gavList as gavIds
 MATCH p = (topLevel)-[*1..]->(dependency)
+WHERE topLevel.id in gavIds AND dependency.id in gavIds AND
+ANY (gavId in gavIds WHERE (topLevel:GAV)-[:DEPENDS_ON*1..]->(dependency:GAV{id:gavId}))
+RETURN DISTINCT dependency.id AS dependencyId"""
+
+  private val _GetDependenciesFromListv2: String =
+    """WITH $gavList as gavIds
+MATCH p = (topLevel:GAV)-[:DEPENDS_ON*1..]->(dependency:GAV)
 WHERE topLevel.id in gavIds AND dependency.id in gavIds AND
 ANY (gavId in gavIds WHERE (topLevel:GAV)-[:DEPENDS_ON*1..]->(dependency:GAV{id:gavId}))
 RETURN DISTINCT dependency.id AS dependencyId"""
